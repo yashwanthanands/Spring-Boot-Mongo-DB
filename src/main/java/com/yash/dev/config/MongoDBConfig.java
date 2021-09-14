ecb753825c4cba0abde9686a8ffd51e3ac258612
Yashwanth Anand S <yashwanthanands@outlook.com>
2021-09-14 18:19:02 +0530

    Commit with MongoDB Configuration

---
+++ src/main/java/com/yash/dev/config/MongoDBConfig.java
@@ -0,0 +1,27 @@
+package com.yash.dev.config;
+
+import com.yash.dev.document.Users;
+import com.yash.dev.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+/**
+ * Created by Yashwanth Anand S
+ **/
+
+@EnableMongoRepositories(basePackageClasses = UserRepository.class)
+@Configuration
+public class MongoDBConfig {
+
+
+    @Bean
+    CommandLineRunner commandLineRunner(UserRepository userRepository) {
+        return strings -> {
+            userRepository.save(new Users(1, "Peter", "Development", 3000L));
+            userRepository.save(new Users(2, "Sam", "Operations", 2000L));
+        };
+    }
+
+}
---
+++ src/main/java/com/yash/dev/controller/UsersResource.java
@@ -0,0 +1,29 @@
+package com.yash.dev.controller;
+
+import com.yash.dev.document.Users;
+import com.yash.dev.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by Yashwanth Anand S
+ **/
+
+@RestController
+@RequestMapping("/rest/users")
+public class UsersResource {
+
+    private UserRepository userRepository;
+
+    public UsersResource(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/all")
+    public List<Users> getAll() {
+        return userRepository.findAll();
+    }
+}
